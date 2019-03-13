2871d524f98f937923f86d3dcd76d7c904a27296
Kumar Ayush <142ayushkumar@gmail.com>
2019-03-13 23:50:41 +0530

    Updated .gitignore to ignore flask session and cache and added basic route

--- application.py
+++ application.py
@@ -8,8 +8,25 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-@app.route("/")
+@app.route("/", method=['POST'])
 def index():
-    render_template('index.html')
+    # check if form is submitted
+    if 'submit' in request.form:
+        #extracting information from forms
+        query = request.form['query']
+        date = request.form['date']
+        category = request.form['category']
+        acts = request.form['acts']
+        judge = request.form['judge']
+        # get results
+        output = give_result(query=query, date=date, category=category, acts=acts, judge=judge)
+        # pass the list to be displayed to index.html and render index.html
+        render_template('index.html', output=output)
+    else
+        #check ui when there is output and no output, forms should be centered when there is no output
+        render_template('index.html')
 
+@app.route("/cases/<string:case_id>", methods = ['GET'])
+def cases(case_id):
+    render_template('case.html')
 
