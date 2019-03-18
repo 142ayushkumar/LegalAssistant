f8c780942f92d75754bc3afac42de4d5c15cc8d7
Kumar Ayush <142ayushkumar@gmail.com>
2019-03-18 22:15:51 +0530

    Setting up backend

--- application.py
+++ application.py
@@ -8,25 +8,33 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+def get_result(query, category, acts, judge, start_date, end_date):
+    return 1
+
 @app.route("/", methods = ['GET', 'POST'])
 def index():
     # check if form is submitted
-    if 'submit' in request.form:
+    data = request.method
+    print(data)
+    if 'search' in request.form:
+        print("here")
         #extracting information from forms
         query = request.form['query']
-        date = request.form['date']
         category = request.form['category']
         acts = request.form['acts']
         judge = request.form['judge']
+        start_date = request.form['from']
+        end_date = request.form['to']
+        print(f"query is {query}, category is {category}, acts is {acts}, judge is {judge}, start date is {start_date}")
         # get results
-        output = give_result(query=query, date=date, category=category, acts=acts, judge=judge)
+        output = get_result(query=query, category=category, acts=acts, judge=judge, start_date=start_date, end_date=end_date)
         # pass the list to be displayed to index.html and render index.html
-        render_template('index.html', output=output)
+        return render_template('index.html', output=output)
     else :
         #check ui when there is output and no output, forms should be centered when there is no output
-        render_template('index.html')
+        return render_template('index.html')
 
 @app.route("/cases/<string:case_id>", methods = ['GET', 'POST'])
 def cases(case_id):
-    render_template('case.html', )
+    return render_template('case.html', case_id )
 
