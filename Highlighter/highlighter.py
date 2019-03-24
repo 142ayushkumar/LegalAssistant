89ad20dd2280b6eecd786c3327ed3b14a65885e2
Piyush Rathi <piyushrathipr@gmail.com>
2019-03-24 18:16:14 +0530

    highlighter v2.0

---
+++ Highlighter/highlighter.py
@@ -0,0 +1,49 @@
+import re
+
+def highlighter_function(file_name, list_of_acts_file):
+	all_words = []
+	fd = open(file_name)
+	fd2 = open(list_of_acts_file)
+
+	all_acts = []
+
+	for line in fd2:
+		if line != '\n' and line!= "":
+			# print(len(line[:-1]))
+			all_acts.append(line[:-1])
+
+	segment_index = []
+	caseName_index = []
+
+	line_no = 0
+
+	for line in fd:
+		temp_words = re.split(', | |\. |\.', line)
+
+		for word_no in range(len(temp_words)):
+			if temp_words[word_no][-3:] =="-->":
+				segment_index.append((line_no, word_no))
+
+		for word_no in range(len(temp_words)):
+			
+			if temp_words[word_no] == "Act" and word_no <= len(temp_words)-2:
+				if word_no + 2 >= len(temp_words) or temp_words[word_no+2][-3:] != "-->":
+					char = temp_words[word_no+1][0]
+					if char >= '0' and char <= '9':
+						start_word = word_no
+						# while start_word >= 0:
+						# 	start_word--
+							
+						caseName_index.append((line_no, start_word, word_no))
+
+		line_no += 1
+
+
+	print(segment_index)
+	print(caseName_index)
+
+	return segment_index, caseName_index
+
+if __name__ == "__main__":
+
+	highlighter_function("Act.txt", "all_acts_central_state.txt")
